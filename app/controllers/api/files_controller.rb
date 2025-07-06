9e160065bed1189e69a5839fc94545bfd5752993
Jacek Podkanski <ruby.object@googlemail.com>
2025-07-06 20:34:39 +0100

    testing the api

--- app/controllers/api/files_controller.rb
+++ app/controllers/api/files_controller.rb
@@ -3,14 +3,20 @@ class Api::FilesController < ApplicationController
 
   def list
     params.permit!
-    Rails.logger.info 'params-----------------------'
+    Rails.logger.info "params-----------------------"
     Rails.logger.info params.inspect
 
     # debugger
     # 1==1
 
     # we do not have pwd yet, but try list the / files
-    files = Dir.open('/').entries.sort
-    render json: files.to_json
+    files = Dir.open(params["pwd"]).entries.sort
+    files_filtered =
+      if params["show_hidden"]
+        files
+      else
+        files.reject{ |f| f.start_with?('.') }
+      end
+    render json: files_filtered.to_json
   end
 end
