5e35c93c9f83663fb39328a7cbd0e722bc11249a
Jacek Podkanski <ruby.object@googlemail.com>
2025-07-06 20:03:27 +0100

    progress with cors

--- app/controllers/api/files_controller.rb
+++ app/controllers/api/files_controller.rb
@@ -1,9 +1,8 @@
 class Api::FilesController < ApplicationController
-
+  skip_before_action :verify_authenticity_token
 
   def list
     # we do not have pwd yet, but try list the / files
-    render :json ["/", [ ".", "zzz" ]]
+    render json: [ "/", [ ".", "this", "is", "a", "sample" ] ].to_json
   end
-
 end
