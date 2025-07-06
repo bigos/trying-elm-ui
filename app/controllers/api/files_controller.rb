a71243b5a8cba35bf645bfee666ef5c936a2f299
Jacek Podkanski <ruby.object@googlemail.com>
2025-07-06 20:28:19 +0100

    correct way of passing parameters in restclient

--- app/controllers/api/files_controller.rb
+++ app/controllers/api/files_controller.rb
@@ -2,7 +2,15 @@ class Api::FilesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def list
+    params.permit!
+    Rails.logger.info 'params-----------------------'
+    Rails.logger.info params.inspect
+
+    # debugger
+    # 1==1
+
     # we do not have pwd yet, but try list the / files
-    render json: [ "/", [ ".", "this", "is", "a", "sample" ] ].to_json
+    files = Dir.open('/').entries.sort
+    render json: files.to_json
   end
 end
