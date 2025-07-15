d9baf061b893ebc017fc889534e8f34f379e0c1d
Jacek Podkanski <ruby.object@googlemail.com>
2025-07-15 17:59:15 +0100

    rm and link

--- copy_files.sh
+++ copy_files.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-cp -v /home/jacek/Programming/Pyrulis/Purescript/halogen//counter/prod/index.js ./public/index_halogen.js
-cp -v /home/jacek/Programming/Pyrulis/Purescript/flame/counter/prod/index.js ./public/index_flame.js
+rm -v ./public/index_halogen.js
+ln -s /home/jacek/Programming/Pyrulis/Purescript/halogen//counter/prod/index.js ./public/index_halogen.js
+
+rm -v ./public/index_flame.js
+ln -s /home/jacek/Programming/Pyrulis/Purescript/flame/counter/prod/index.js ./public/index_flame.js
