51595472e10061f575354c19dfac92659706de0a
Jacek Podkanski <ruby.object@googlemail.com>
2025-07-19 23:52:20 +0100

    linking files

--- copy_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm -v ./public/index_halogen.js
-ln -s /home/jacek/Programming/Rails/trying-elm-ui/vendor/purescript/halogen/counter/prod/index.js  ./public/index_halogen.js
-
-rm -v ./public/index_flame.js
-ln -s /home/jacek/Programming/Rails/trying-elm-ui/vendor/purescript/flame/counter/prod/index.js ./public/index_flame.js
