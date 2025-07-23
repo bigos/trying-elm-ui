ddd4a3d1f76f32d0bf078d16fffd732ae7ea0e8c
Jacek Podkanski <ruby.object@googlemail.com>
2025-07-23 20:19:36 +0100

    removed unused elements

--- vendor/purescript/flame/counter/src/Main.purs
+++ vendor/purescript/flame/counter/src/Main.purs
@@ -1,8 +1,7 @@
 -- | Counter example using side effects free updating
 module Main where
 
-import Prelude (class Eq, class Show, Unit, bind, discard, map, negate, show, ($), (+), (-), (<), (<$>), (<*>), (<<<), (<>), (==))
---import Prelude
+import Prelude
 
 import Affjax.RequestBody (json)
 import Affjax.ResponseFormat as AR
@@ -31,10 +30,7 @@ import Web.HTML.Window (document)
 -- *TYPES --
 
 type Model =
-  { url ∷ String
-  , result ∷ Result
-  , counter :: Int
-  , flags :: Flags
+  { flags :: Flags
   , dirs :: Dirs
   , resultFiles :: ResultFiles
   }
@@ -60,7 +56,7 @@ type FetchingFilePost =
   , show_hidden :: Boolean
   }
 
-data Message = UpdateUrl String | Fetch | Increment | Decrement | Initialize Flags | FetchFiles
+data Message = Initialize Flags | FetchFiles
 
 data Result = NotFetched | Fetching | Ok String | Error String
 data ResultFiles = NotFetchedFile | FetchingFile | OkFile Files | ErrorFile String
@@ -87,10 +83,7 @@ instance showResultFiles :: Show (ResultFiles) where
 
 init ∷ Model
 init =
-  { url: "https://httpbin.org/get"
-  , result: NotFetched
-  , counter: 0
-  , flags:
+  { flags:
       { counter_start: Nothing
       , base_url: Nothing
       , logname: Nothing
@@ -113,19 +106,7 @@ update ∷ AffUpdate Model Message
 update { display, model, message } =
   case message of
     Initialize flags -> FAE.diff
-      { url: model.url
-      , result: model.result
-      , counter: flagsCounter flags
-      , flags: flags
-      }
-    UpdateUrl url → FAE.diff
-      { url, result: NotFetched }
-    Fetch → do
-      display $ FAE.diff' { result: Fetching }
-      response ← A.get AR.string model.url
-      FAE.diff <<< { result: _ } $ case response of
-        Left error → Error $ A.printError error
-        Right payload → Ok payload.body
+      { flags: flags }
     FetchFiles -> do
       display $ FAE.diff'
         { resultFiles: FetchingFile }
@@ -147,11 +128,6 @@ update { display, model, message } =
                     OkFile (f)
             }
 
-    Increment -> FAE.diff
-      { url: model.url, result: model.result, counter: model.counter + 1 }
-    Decrement -> FAE.diff
-      { url: model.url, result: model.result, counter: model.counter - 1 }
-
 flagsCounter :: Flags -> Int
 flagsCounter flags =
   fromMaybe (-5) (fromString (fromMaybe "" flags.counter_start))
@@ -161,43 +137,21 @@ bgColor :: Int -> String
 bgColor counter = if counter < 0 then "red" else "lime"
 
 view ∷ Model → Html Message
-view model -- { url, result, counter, flags, dirs, resultFiles }
-  = HE.main "main"
-  [ HE.button [ HA.onClick Decrement ] "-"
-  , HE.span
-      [ HA.styleAttr
-          ( "background-color: "
-              <> (bgColor model.counter)
-              <> "; "
-              <> "margin: auto 1em;"
-          )
-      ]
-      [ HE.text (show model.counter) ]
-  , HE.button [ HA.onClick Increment ] "+"
-  , HE.br
-  , HE.input [ HA.onInput UpdateUrl, HA.value model.url, HA.type' "text" ]
-  , HE.button [ HA.onClick Fetch, HA.disabled $ model.result == Fetching ] "Fetch"
-  , case model.result of
-      NotFetched →
-        HE.div_ "Not Fetched..."
-      Fetching →
-        HE.div_ "Fetching..."
-      Ok ok →
-        HE.pre_ <<< HE.code_ $ "Ok: " <> ok
-      Error error →
-        HE.div_ $ "Error: " <> error
-  , HE.div (da_border_red)
-      [ HE.div (da_border_green) "general toolbar"
-      , HE.div [ HA.styleAttr "display: inline-flex" ]
-          [ HE.div da_border_red (panel "left")
-          , HE.div da_border_green (panel "right")
+view model = HE.main "main"
+  [ HE.div da_border_green
+      [ HE.div (da_border_red)
+          [ HE.div (da_border_green) "general toolbar"
+          , HE.div [ HA.styleAttr "display: inline-flex" ]
+              [ HE.div da_border_red (panel "left")
+              , HE.div da_border_green (panel "right")
+              ]
           ]
+      , HE.h3_ "flags"
+      , HE.p_ (show model.flags)
+      , HE.h3_ "model"
+      , HE.p_ (show model)
+      , HE.button [ HA.onClick FetchFiles, HA.disabled $ model.resultFiles == FetchingFile ] "Fetch Fieles"
       ]
-  , HE.h3_ "flags"
-  , HE.p_ (show model.flags)
-  , HE.h3_ "model"
-  , HE.p_ (show model)
-  , HE.button [ HA.onClick FetchFiles, HA.disabled $ model.resultFiles == FetchingFile ] "Fetch Fieles"
   ]
 
 names :: Array String
