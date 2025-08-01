f1412636d2cb653a5c6acbae2419881e4b303b7b
Jacek Podkanski <ruby.object@googlemail.com>
2025-08-02 00:42:13 +0100

    example file data

--- app/models/concerns/files.rb
+++ app/models/concerns/files.rb
@@ -2,7 +2,21 @@ class Files
   def initialize(pwd, show_hidden)
     @dir = Dir.open pwd
     @show_hidden = show_hidden
-    @files = @dir.entries.sort
+    @files = @dir
+               .entries
+               .sort
+               .map { |f|  File.new(File.join(@dir.path, f)) }
+               .map { |f|
+      { name: File.basename(f),
+        executable: File.executable?(f),
+        extname: File.extname(f),
+        ftype: File.ftype(f),
+        size: File.size(f),
+        mtime: File.stat(f).mtime,
+        mode: File.stat(f).mode,
+        symlink: File.stat(f).symlink?
+      }
+    }
   end
 
   # puts Files.new('/home/jacek/', false).data.to_json
@@ -13,7 +27,7 @@ class Files
         if @show_hidden
           @files
         else
-          @files.reject { |f| f.start_with?(".") }
+          @files.reject { |f|  f[:name].start_with?(".") }
         end
     }
   end
