72c898eb0a0cce8146d1f4445396210e0988d42f
Jacek Podkanski <ruby.object@googlemail.com>
2025-11-04 23:29:45 +0000

    star as origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,6 +2,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    # origins '127.0.0.1:3000'
+    # origins 'localhost:3000'
     origins '*'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
