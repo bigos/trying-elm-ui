66092c7a49c4eda5ba8cd474f6044ebff5990634
Jacek Podkanski <ruby.object@googlemail.com>
2025-11-05 20:12:57 +0000

    cors again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,20 @@
 # config/initializers/cors.rb
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    # origins '127.0.0.1:3000'
-    # origins 'localhost:3000'
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+  if Rails.env.development?
+    Rails.logger.info "using development cors"
+    allow do
+      # origins '127.0.0.1:3000'
+      # origins 'localhost:3000'
+      origins '*'
+      resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    end
+  else
+    Rails.logger.info "using production cors"
+    domain = 'https://localhost:5000'
+    allow do
+      origins domain
+      resource domain, headers: :any, methods: [:get, :post, :patch, :put]
+    end
   end
 end
