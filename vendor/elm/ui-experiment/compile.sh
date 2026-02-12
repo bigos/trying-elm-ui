8ed3e144e643665568c3e189007a01e495b38c5c
Jacek Podkanski <ruby.object@googlemail.com>
2026-02-12 23:01:42 +0000

    mv -v

--- vendor/elm/ui-experiment/compile.sh
+++ vendor/elm/ui-experiment/compile.sh
@@ -3,4 +3,4 @@
 echo Compiling
 
 elm make ./src/UiExperiment.elm --output ui-experiment.js
-mv ./ui-experiment.js ../../../public/ui-experiment.js
+mv -v ./ui-experiment.js ../../../public/ui-experiment.js
